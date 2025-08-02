45b2487a2d3f8b91c3c08b65e74136d638cf5c99
Turab45 <turabbajeer202@gmail.com>
2025-08-03 00:25:03 +0200

    add language models (LLMs)

--- langchan/chat_models.py
+++ langchan/chat_models.py
@@ -6,6 +6,7 @@ import os
 load_dotenv()
 
 # Chat Models
+# temp value is used to control the randomness or the creativity of the model's responses
 model = ChatOpenAI(model="gpt-4o", temperature=0.7, openai_api_key=os.getenv("OPENAI_API_KEY"))
 
 result = model.invoke("What is the capital of France?")
