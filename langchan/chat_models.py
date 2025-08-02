68ae5149423f14f290eca5ed90f11b64458fb01a
Turab45 <turabbajeer202@gmail.com>
2025-08-03 00:22:49 +0200

    add language models (LLMs)

---
+++ langchan/chat_models.py
@@ -0,0 +1,13 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file (API key)
+load_dotenv()
+
+# Chat Models
+model = ChatOpenAI(model="gpt-4o", temperature=0.7, openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+result = model.invoke("What is the capital of France?")
+
+print(result.content)  # Access the content of the response
