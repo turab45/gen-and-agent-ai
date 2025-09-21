7c834bd8b37ea5f5b770c37bcb48d6001287d4b6
Turab45 <turabbajeer202@gmail.com>
2025-09-21 22:33:08 +0200

    Add another chain for the medical assistatnt agent to summarize the output in a few lines

--- langchan/sequential_runnable.py
+++ langchan/sequential_runnable.py
@@ -26,3 +26,22 @@ response = chain.invoke({"symptoms": "Fever, cough, shortness of breath"})
 
 print("AI:", response)
 
+# This is just a simple example of a sequential runnable chain.
+# We expand it by adding another chain to summarize the response in a few lines.
+print("##############################################################################")
+print("Now summarizing the diagnosis and treatment plan:")
+print("##############################################################################")
+
+
+template2 = PromptTemplate(
+    template = "Summarize the following medical diagnosis and treatment plan in a few lines:\n\n" \
+    "{diagnosis_and_treatment}\n\n",
+    input_variables = ["diagnosis_and_treatment"]
+)
+
+chain2 = RunnableSequence(template2, model, parser)
+
+response2 = chain2.invoke({"diagnosis_and_treatment": response})
+
+
+print("AI:", response2)
