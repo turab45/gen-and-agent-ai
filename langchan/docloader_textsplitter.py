a156bbe864947742a5aca643716d3adb06bc4c48
Turab45 <turabbajeer202@gmail.com>
2025-09-24 21:36:15 +0200

    Add document loader and text splitter

---
+++ langchan/docloader_textsplitter.py
@@ -0,0 +1,17 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+
+# load the pdf document
+loader = PyPDFLoader("Eye_tracking_H4.pdf")
+pages = loader.load()
+print(f"Total Pages: {len(pages)}")
+
+splitter = CharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=0,
+    separator="")
+
+docs = splitter.split_documents(pages)
+print(f"Total Chunks: {len(docs)}")
+print(docs[0].page_content)
